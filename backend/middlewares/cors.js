e692366bf9953af7ebdbc9a22feea87274af8fa0
Ashray <vbashray@gmail.com>
2025-05-02 00:03:38 +0530

    added hostname service to the code.

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,8 +1,8 @@
-// middlewares/cors.js
+// backend/middlewares/cors.js
 import cors from 'cors';
 
 const corsOptions = {
-  origin: ['http://localhost:3000', 'http://localhost:5173'], // ✅ Allow both
+  origin: ['http://localhost:3000', 'http://localhost:5173', 'https://nutritrackr.netlify.app'],
   credentials: true,
 };
 
